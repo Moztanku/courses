2e898106c25098631b0abcc1fd8b45c92f59949d
Moztanku <Mostankpl@gmail.com>
2024-06-17 19:59:01 +0200

    AiSD lab-5 z1

--- AiSD/lab-5/src/test.cpp
+++ AiSD/lab-5/src/test.cpp
@@ -1,82 +1,212 @@
 #include <iostream>
 
+#include <optional>
 #include <vector>
 #include <memory>
 #include <random>
+#include <algorithm>
+#include <functional>
 
-class Vertex
+constexpr double INF = std::numeric_limits<double>::infinity();
+
+class Graph
 {
     public:
-        Vertex() : m_id(s_id++) {}
+        Graph(const size_t size, const std::optional<double> initial = std::nullopt) : m_size{size}
+        {
+            m_adjMat.resize(m_size);
 
-        auto id() const -> int { return m_id; }
+            // Resize the adjacency matrix to store only the upper triangular part, no need to store [i][j] and [j][i]
+            for (size_t i = 0; i < m_size; i++)
+            {
+                m_adjMat[i].resize(
+                    m_size - i - 1
+                );
+            }
+
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_real_distribution<> dis(0.0, 1.0);
+
+            // Fill the adjacency matrix with random values (or the initial value if provided)
+            for (size_t i = 0; i < m_size; i++)
+                for (size_t j = i + 1; j < m_size; j++)
+                    if (initial.has_value())
+                        m_adjMat[i][j - i - 1] = initial.value();
+                    else
+                        m_adjMat[i][j - i - 1] = dis(gen);
+        }
+
+        auto size() const -> size_t
+        {
+            return m_size;
+        }
+
+        auto get(size_t i, size_t j) const -> double
+        {
+            if (i == j)
+                return 0.0;
+
+            if (i > j)
+                std::swap(i, j);
+
+            return m_adjMat[i][j - i - 1];
+        }
+
+        auto set(size_t i, size_t j, double val) -> void
+        {
+            if (i == j)
+                return;
+
+            if (i > j)
+                std::swap(i, j);
+
+            m_adjMat[i][j - i - 1] = val;
+        }
+
     private:
-        const int m_id;
-        static int s_id;
+        const size_t m_size;
+        std::vector<std::vector<double>> m_adjMat;
 };
 
-int Vertex::s_id = 0;
-
-struct Edge
+auto prim(const Graph& graph) -> Graph
 {
-    Edge(const Vertex* u, const Vertex* v, double weight) : u(u), v(v), weight(weight) {}
+    // Lambda function to find the closest node to the visited nodes
+    constexpr auto find_closest = [](const Graph& graph, const std::vector<bool>& visited)
+    {
+        struct Closest
+        {
+            size_t i{0};
+            size_t j{0};
+            double val{INF};
+        } min;
 
-    const Vertex* u;
-    const Vertex* v;
-    const double weight;
-};
+        for (size_t i = 0; i < graph.size(); i++)
+        {
+            if (!visited[i])
+                continue;
 
-auto connect(const Vertex& u, const Vertex& v) -> Edge
-{
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_real_distribution<> dis(0, 1);
+            for (size_t j = 0; j < graph.size(); j++)
+            {
+                if (visited[j])
+                    continue;
 
-    return Edge(&u, &v, dis(gen));
+                if (graph.get(i, j) < min.val)
+                {
+                    min.val = graph.get(i, j);
+                    min.i = i;
+                    min.j = j;
+                }
+            }
+        }
+
+        return min;
+    };
+
+    // Create a new graph to store the minimum spanning tree, initially filled with INF to represent no edge
+    Graph mst(graph.size(), INF);
+
+    std::vector<bool> visited(graph.size(), false);
+    int visited_count = 0;
+
+    visited[0] = true;
+    visited_count++;
+
+    // Find the closest node to the current mst until all nodes are visited
+    while (visited_count < graph.size())
+    {
+        auto min = find_closest(graph, visited);
+
+        mst.set(min.i, min.j, graph.get(min.i, min.j));
+
+        visited[min.j] = true;
+        visited_count++;
+    }
+
+    return mst;
 }
 
-auto prims(const std::vector<Vertex>& V, const std::vector<Edge>& E) -> std::vector<Edge>
+auto kruskal(const Graph& graph) -> Graph
 {
-    // constexpr auto find_closest = [](const std::vector<Vertex>& V, const std::vector<Edge>& E, const std::vector<Edge>& T) -> Edge
-    // {
+    struct Edge
+    {
+        size_t src, dest;
+        double weight;
+    };
 
-    // }
+    // Create a vector to store the edges
+    std::vector<Edge> edges;
 
-    std::vector<Edge> T;
+    // Fill the vector with all the edges in the graph
+    for (size_t i = 0; i < graph.size(); i++)
+    {
+        for (size_t j = i + 1; j < graph.size(); j++)
+        {
+            if (graph.get(i, j) != INF)
+            {
+                edges.push_back({i, j, graph.get(i, j)});
+            }
+        }
+    }
 
-    const Vertex* v = &V[0];
-    Vertex* closest = nullptr;
+    // Sort the edges by weight
+    std::sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b)
+    {
+        return a.weight < b.weight;
+    });
 
-    // while (T.size() < V.size() - 1)
-    // {
+    // Create a vector to store the parent of each node
+    std::vector<size_t> parent(graph.size());
+    std::iota(parent.begin(), parent.end(), 0);
 
-    // }
+    // Function to find the parent of a node
+    const std::function<size_t(size_t)> find = [&parent, &find](size_t i)
+    {
+        if (parent[i] == i)
+        {
+            return i;
+        }
+        return parent[i] = find(parent[i]);
+    };
 
-    return T;
-}
+    // Function to union two nodes
+    auto union_nodes = [&parent, &find](size_t i, size_t j)
+    {
+        parent[find(i)] = find(j);
+    };
 
-auto kruskals(const std::vector<Vertex>& V, const std::vector<Edge>& E) -> std::vector<Edge>
-{
-    std::vector<Edge> T;
+    // Create a new graph to store the minimum spanning tree
+    Graph mst(graph.size(), INF);
 
-    // while (T.size() < V.size() - 1)
-    // {
+    // Add the edges to the minimum spanning tree
+    for (const auto& edge : edges)
+    {
+        if (find(edge.src) != find(edge.dest))
+        {
+            mst.set(edge.src, edge.dest, edge.weight);
+            union_nodes(edge.src, edge.dest);
+        }
+    }
 
-    // }
-
-    return T;
+    return mst;
 }
 
 auto main() -> int
 {
-    constexpr size_t N = 100;
+    Graph full_graph(100);
 
-    // G = (V, E)
-    std::vector<Vertex> V(100);
-    std::vector<Edge> E;
-    E.reserve((N * (N - 1)) / 2);
+    Graph mst_prim = prim(full_graph);
+    Graph mst_kruskal = kruskal(full_graph);
 
-    for (size_t i = 0; i < N; i++)
-        for (size_t j = i + 1; j < N; j++)
-            E.push_back(connect(V[i], V[j]));
+    size_t counter = 0;
+    for (size_t i = 0; i < full_graph.size(); i++)
+        for (size_t j = i + 1; j < full_graph.size(); j++)
+            if (mst_prim.get(i, j) != INF)
+                std::cout << ++counter << ". " << i << " " << j << " " << mst_prim.get(i, j) << std::endl;
+    
+    counter = 0;
+    for (size_t i = 0; i < full_graph.size(); i++)
+        for (size_t j = i + 1; j < full_graph.size(); j++)
+            if (mst_kruskal.get(i, j) != INF)
+                std::cout << ++counter << ". " << i << " " << j << " " << mst_kruskal.get(i, j) << std::endl;
 }
